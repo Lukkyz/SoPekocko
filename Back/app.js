3e70ad9eef00e31ea693d869bec0c005927b64de
Lukkyz <bienaimelucas@hotmail.fr>
2020-07-26 16:38:16 +0200

    Add .env

--- Back/app.js
+++ Back/app.js
@@ -12,6 +12,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("Connexion réussie"))
   .catch(() => console.log("Connexion echouée"));
